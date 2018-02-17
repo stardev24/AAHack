caf0b8d17b0f3ca5dc1db93bc2027e70f680980f
stardev24 <stardev24@gmail.com>
2018-02-17 13:23:24 +0530

    Added solution document

--- hack/app/client/app/account/bookings/index.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-const angular = require('angular');
-import BookingController from './booking.controller';
-
-
-console.log("----------------->debug",BookingController)
-export default angular.module('airAsiaAssignmentApp.bookings', [])
-  .controller('BookingController', BookingController)
-  .name;
--- hack/app/client/components/MyBookings/index.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-const angular = require('angular');
-import MyBookingsController from './MyBookings.controller';
-
-export default angular.module('airAsiaAssignmentApp.bookings', [])
-  .controller('MyBookingsController', MyBookingsController)
-  .name;
